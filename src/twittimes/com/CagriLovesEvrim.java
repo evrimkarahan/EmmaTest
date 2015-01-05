fd1908911eecb8f6a57ef65d84430f8b0ec336e5
cagrikarahan <cagrikarahan@gmail.com>
2015-01-04 22:25:33 -0800

    test

---
+++ src/twittimes/com/CagriLovesEvrim.java
@@ -0,0 +1,10 @@
+package twittimes.com;
+
+public class CagriLovesEvrim {
+
+	public static void main(String[] args) {
+		for(int i=0; i< 1000000; i++)
+			System.out.println("Cagri loves Evrim");
+	}
+
+}
